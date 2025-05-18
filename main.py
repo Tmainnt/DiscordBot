a034902d632df4e6ecde8da64e55ab4244a96f51
Tmainnt <gamerthann@gmail.com>
2025-05-18 17:13:23 +0700

    fixed error on line 60.

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ async def on_message(message):
     if message.author == bot.user:
         return
     
-    if message.content.startwith("!"):
+    if message.content.startswith("!"):
         await bot.process_commands(message)
         return
     #print(f"Message received: {message.content}")
